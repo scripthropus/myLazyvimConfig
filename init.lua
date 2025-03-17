69d7eacb0e85bdbc26e9392e1265da31a9da679e
scripthropus <script2097152@gmail.com>
2025-03-17 16:41:14 +0900

    ターミナルをgit bashにした

--- init.lua
+++ init.lua
@@ -3,3 +3,9 @@ require("config.lazy")
 
 -- オプション: カラースキームを設定
 vim.cmd.colorscheme("cyberdream")
+if vim.fn.has('win32') == 1 then
+  vim.opt.shell = "bash"
+  vim.opt.shellcmdflag = "-c"
+  vim.opt.shellquote = "\""
+  vim.opt.shellxquote = ""
+end
